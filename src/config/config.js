8ae1f3c6f2d3a7f5e52f6a36009bfd03fcf8865a
holyaustin <holyaustin@yahoo.com>
2023-03-02 20:32:41 +0100

    config commit

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,14 @@
 import { config } from "@onflow/fcl"
 
 config({
-  "accessNode.api": 'https://rest-testnet.onflow.org',
+  "accessNode.api": "https://rest-testnet.onflow.org",
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
+  "0xFungibleToken": "0x9a0766d93b6608b7",
+  "0xFUSD": "0xe223d8a629e49c68",
+  "0xDappy": "0xdb3d539e48a805b7",
   //"accessNode.api": process.env.REACT_APP_ACCESS_NODE,
   //"discovery.wallet": process.env.REACT_APP_WALLET_DISCOVERY,
-  "discovery.wallet": 'https://fcl-discovery.onflow.org/testnet/authn',
-  //"0xFungibleToken": "0x9a0766d93b6608b7",
-  //"0xFUSD": "0xe223d8a629e49c68",
-  //"0xDappy": "0xdb3d539e48a805b7"
-  "0xFungibleToken": process.env.REACT_APP_FT_CONTRACT,
-  "0xFUSD": process.env.REACT_APP_FUSD_CONTRACT,
-  "0xDappy": process.env.REACT_APP_DAPPY_CONTRACT
+  //"0xFungibleToken": process.env.REACT_APP_FT_CONTRACT,
+  //"0xFUSD": process.env.REACT_APP_FUSD_CONTRACT,
+  //"0xDappy": process.env.REACT_APP_DAPPY_CONTRACT
 })
