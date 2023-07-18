0c8bd0826a4e55f675d0a9e6104877e9b432b2c9
holyaustin <holyaustin@yahoo.com>
2023-07-18 10:42:13 +0100

    nft update

--- src/config/routes.config.js
+++ src/config/routes.config.js
@@ -7,8 +7,8 @@ import Designer from '../pages/Designer.page'
 
 export const ROUTES = [
   { name: "Home", path: "/", component: Home, nav: true },
-  { name: "Soldier-Ants", path: "/dappies", component: Dappies, nav: true },
-  { name: "Colonies", path: '/packs', component: Packs, nav: true },
+  { name: "Soldier-Ants", path: "/soldier-ant", component: Dappies, nav: true },
+  { name: "Colonies", path: '/colonies', component: Packs, nav: true },
   { name: "Collection", path: '/collection', component: Collection, nav: true },
   { name: "PackDetails", path: '/packs/:packID', component: PackDetails, nav: false },
   { name: "Designer", path: '/designer', component: Designer, nav: false }
